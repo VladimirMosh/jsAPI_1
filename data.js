24e3a16e781daefae92dfded423d46aa7223544d
VladimirMosh <the.uncle.mosh@gmail.com>
2024-12-08 22:24:37 +0300

    Complate

---
+++ data.js
@@ -0,0 +1,67 @@
+
+export const training = `
+[
+    {
+        "id": "1",
+        "name_of_workout": "Йога",
+        "time_of_workout": "09:30 - 11:00",
+        "max_number_of_participants": "15",
+        "current_number_of_participants": "15"
+    },
+    {
+        "id": "2",
+        "name_of_workout": "Кардиотренировка",
+        "time_of_workout": "11:00 - 11:45",
+        "max_number_of_participants": "10",
+        "current_number_of_participants": "3"
+    },
+    {
+        "id": "3",
+        "name_of_workout": "Пилатес",
+        "time_of_workout": "11:00 - 11:50",
+        "max_number_of_participants": "20",
+        "current_number_of_participants": "18"
+    },
+    {
+        "id": "4",
+        "name_of_workout": "Body Up",
+        "time_of_workout": "10:00 - 10:55",
+        "max_number_of_participants": "25",
+        "current_number_of_participants": "20"
+    },
+    {
+        "id": "5",
+        "name_of_workout": "Водные тренировки",
+        "time_of_workout": "11:00 - 11:45",
+        "max_number_of_participants": "3",
+        "current_number_of_participants": "3"
+    },
+    {
+        "id": "6",
+        "name_of_workout": "Ноги и ягодицы",
+        "time_of_workout": "10:00 - 10:55",
+        "max_number_of_participants": "20",
+        "current_number_of_participants": "19"
+    },
+    {
+        "id": "7",
+        "name_of_workout": "Функциональная тренировка",
+        "time_of_workout": "09:30 - 11:00",
+        "max_number_of_participants": "20",
+        "current_number_of_participants": "20"
+    },
+    {
+        "id": "8",
+        "name_of_workout": "Только для женщин",
+        "time_of_workout": "09:00 - 09:55",
+        "max_number_of_participants": "25",
+        "current_number_of_participants": "7"
+    },
+    {
+        "id": "9",
+        "name_of_workout": "ФИТНЕС-МИКС",
+        "time_of_workout": "11:30 - 12:00",
+        "max_number_of_participants": "20",
+        "current_number_of_participants": "14"
+    }
+]`;
